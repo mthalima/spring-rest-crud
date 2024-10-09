c0335df92d14c4860f0987eec8fc7454afc77902
mthalima <matheus.ecomp@gmail.com>
2024-10-09 18:48:06 -0300

    permissoes de acordo com a role

--- JPA Rest controller/src/main/java/com/codandospring/springboot/cruddemo/security/SecurityConfig.java
+++ JPA Rest controller/src/main/java/com/codandospring/springboot/cruddemo/security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.codandospring.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -34,10 +38,28 @@ public class SecurityConfig {
         UserDetails andrea = User.builder()
                 .username("andrea")
                 .password("{noop}123456")
-                .roles("FUNCIONARIO", "CEO")
+                .roles("FUNCIONARIO", "ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(joao, maria, pedro, andrea);
     }
 
+
+    //DEFINE AS LIMITAÇÕES DE A CORDO COM AS PERMISSÕES DE CADA CADASTRO
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("FUNCIONARIO")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("FUNCIONARIO")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("GERENTE")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("GERENTE")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        //USA AUTENTICACAO BASICA HTTP
+        http.httpBasic(Customizer.withDefaults());
+        return null;
+    }
 }
